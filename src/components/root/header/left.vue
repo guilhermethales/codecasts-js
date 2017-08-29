30fea5002e897b9791d1a117ffe58af2a2d44716
Guilherme Thales <guilhermevtog@hotmail.com>
2017-08-29 16:49:39 -0300

    info usuario

--- src/components/root/header/left.vue
+++ src/components/root/header/left.vue
@@ -3,9 +3,6 @@
 		<router-link :to="{name: 'dashboard.main'}" tag="li" exact>
 			<a>Home</a>
 		</router-link>
-		<router-link :to="{name: 'auth'}" tag="li">
-			<a>Login</a>
-		</router-link>
 	</ul>
 </template>
 
---
+++ src/components/root/header/right.vue
@@ -0,0 +1,37 @@
+<template>
+	<ul class="nav navbar-nav navbar-right">
+	<router-link :to="{name: 'auth'}" tag="li" v-if="!isLogged">
+				<a>Login</a>
+			</router-link>
+		<li class="dropdown" :class="{ open: isOpenUser }" v-if="isLogged">
+			<a href="javascript:;" 
+				class="dropdown=toggle" @click="toggleUserMenu()">{{ currentUser.name }} <span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="javascript:;">Action</a></li>
+				<li><a href="javascript:;">Another Action</a></li>
+				<li><a href="javascript:;">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="javascript:;">Separated link</a></li>
+			</ul>
+		</li>
+	</ul>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex'
+	export default {
+		data () {
+			return {
+				isOpenUser: false
+			}
+		},
+		computed: {
+			...mapGetters(['isLogged', 'currentUser'])
+		},
+		methods: {
+			toggleUserMenu () {
+				this.isOpenUser = !this.isOpenUser
+			}
+		}
+	}
+</script>
